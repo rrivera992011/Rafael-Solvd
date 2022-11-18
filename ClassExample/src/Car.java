1cd1ad7d7373cd80ee8d9a8258c1cb64a6489ae4
rrivera992011 <63326993+rrivera992011@users.noreply.github.com>
2022-11-18 00:34:23 -0600

    Added class example folder

---
+++ ClassExample/src/Car.java
@@ -0,0 +1,23 @@
+class Car {
+    private String color;
+    private int productionYear;
+    private float maxSpeed;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+
+        return this.color;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public int getProductionYear() {
+
+        return this.productionYear;
+    }
+}
